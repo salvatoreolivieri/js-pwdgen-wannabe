a2d7520f75c7e6f6d32fe2b7113c12138cce0ebb
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-04-05 16:08:50 +0200

    Seconda variante

--- script.js
+++ script.js
@@ -18,11 +18,12 @@
 const firstName = prompt("Inserisci il tuo Nome");
 const surName = prompt("Inserisci il tuo Cognome");
 const favColor = prompt("Inserisci il tuo Colore Preferito");
+let stupidpassword = "nomecognomecolorepreferito22"
 
 // Stampare il contenuto
 
 document.getElementById("content").innerHTML =
 `
-Ciao ${firstName} ${surName}, il ${favColor} è proprio un bel colore. Ecco la password (insicurissima) che ho generato per te: nomecognomecolorepreferito22
+Ciao ${firstName} ${surName}, il ${favColor} è proprio un bel colore. Ecco la password (insicurissima) che ho generato per te: $(stupidpassowrd)
 `;
 
