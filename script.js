821b4895743e56f8b5648fda6091054ec0ff7a13
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-04-05 15:44:48 +0200

    Mappati obiettivi e macro point da seguire

---
+++ script.js
@@ -0,0 +1,13 @@
+
+//  Chiedere info all'utente che visita la pagina
+
+/*
+  1. Chiedi all’utente il suo nome,
+  2. Chiedi il suo cognome,
+  3. Chiedi il suo colore preferito
+  4. Stampare in pagina
+
+  a. Definire le variabili
+  b. Creare sezione dove stampare il contenuto
+  c. Stampare il contenuto
+*/
